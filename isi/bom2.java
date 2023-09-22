fdc8641bf31a251851fc30b695240745f8cbac98
inaa12 <inah3803@gmail.com>
2023-09-23 07:00:17 +0800

    project pertama

---
+++ isi/bom2.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class bom2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class bom2 extends bom
+{
+    /**
+     * Act - do whatever the bom2 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
